eae732109359e9fc55988c66779c974181aa8439
axenteoctavian <axente.octavian@gmail.com>
2024-02-13 16:54:27 +0200

    data formatter fix 2

--- contracts/deploy/interface.go
+++ contracts/deploy/interface.go
@@ -2,6 +2,6 @@ package deploy
 
 // DataFormatter should format txs data for bridge operations
 type DataFormatter interface {
-	CreateTxsData(bytes []byte) [][]byte
+	CreateTxsData(bytes []byte) []byte
 	IsInterfaceNil() bool
 }
